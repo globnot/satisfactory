d102b5bf0738f35b12977d489de9980c0387d844
AntoineBorda <pro@antoineborda.com>
2024-11-12 10:21:25 +0100

    [BUG] DNS TwitchChatBotService

--- src/Infrastructure/Service/Twitch/TwitchChatBotService.php
+++ src/Infrastructure/Service/Twitch/TwitchChatBotService.php
@@ -50,7 +50,7 @@ class TwitchChatBotService implements TwitchChatBotInterface
                 ],
                 'identity' => [
                     'username' => $this->twitchConfiguration->getUsername(),
-                    'password' => 'oauth:' . $accessToken,
+                    'password' => 'oauth:'.$accessToken,
                 ],
                 'channels' => [$this->twitchConfiguration->getChannel()],
             ]);
